46b0569b5453ceef0a0025e9d25a01e7b9238636
vidit <viditsharma818@gmail.com>
2023-09-11 23:49:09 +0530

    gsap inti

--- src/components/HomeTextAnime.jsx
+++ src/components/HomeTextAnime.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 function HomeTextAnime(props) {
   return (
-    <div ref={props.refe}>
-        <div className='box container w-[100vw] h-[100vh] text-white flex justify-center items-center flex-col gap-4'>
+    <div ref={props.refe} className=' w-[100vw]'> 
+        <div className='box bg-[url(https://www.sisainfosec.com/wp-content/uploads/2022/09/1-1.gif)] bg-cover text-center bg-center container min-w-[100vw] text-black h-[100vh]  flex justify-center items-center flex-col gap-4'>
 
-       <h1 className='h1 text-[2vw]'>Digital transformation is opening doors to newer and evolving threats.</h1>
-       <h1 className='h2 text-[2vw]'>Digital transformation is opening doors to newer and evolving threats.</h1>
-       <h1 className='h3 text-[2vw]'>Digital transformation is opening doors to newer and evolving threats.</h1>
+       <h1 className='h1 text-[3vw] font-semibold'>Digital transformation is opening doors to <br /> newer and evolving threats.</h1>
+       <h1 className='h2 text-[3vw] font-semibold'>Compliant organizations are getting breached, <br /> despite investments in advanced technology <br /> and increasing cyber spends.
+</h1>
+       <h1 className='h3 text-[3vw] font-semibold'>Fueling your digital journey requires <br />
+<span className='text-[#faa731]'>Forensics-driven Cybersecurity.</span></h1>
 
 
         </div>
