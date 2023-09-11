8f16bddba032cde057a5896046ab0ab947a1556b
vidit <viditsharma818@gmail.com>
2023-09-11 23:35:23 +0530

    gsap inti

---
+++ src/components/HomeTextAnime.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function HomeTextAnime(props) {
+  return (
+    <div ref={props.refe}>
+        <div className='box container w-[100vw] h-[100vh] text-white flex justify-center items-center flex-col gap-4'>
+
+       <h1 className='h1 text-[2vw]'>Digital transformation is opening doors to newer and evolving threats.</h1>
+       <h1 className='h2 text-[2vw]'>Digital transformation is opening doors to newer and evolving threats.</h1>
+       <h1 className='h3 text-[2vw]'>Digital transformation is opening doors to newer and evolving threats.</h1>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default HomeTextAnime
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useRef ,useLayoutEffect} from 'react'
 import Navbar from '../components/Navbar';
+import HomeTextAnime from '../components/HomeTextAnime';
+import gsap,{Power3} from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger); 
+
 function Home() {
+  const ref=useRef(null)
+  const tl = useRef()
+  useLayoutEffect(() => {
+
+    let ctx = gsap.context(()=>{
+
+      tl.current = gsap.timeline({
+        scrollTrigger:{
+          trigger:".box",
+          // scroller:"body",
+          markers:true,
+          start:"top top",
+          // end:"bottom bottom",
+          
+          scrub:2,
+          pin:true
+
+        }
+      })
+      tl.current.fromTo(".h1", { opacity:0 ,y:300, },{opacity:1,y:0})
+      tl.current.fromTo(".h1", { opacity:1 , },{opacity:0,})
+
+
+      tl.current.fromTo(".h2", { opacity:0 ,y:300, },{opacity:1,y:0})
+      tl.current.fromTo(".h2", { opacity:1},{opacity:0})
+
+      tl.current.fromTo(".h3", { opacity:0 ,y:300,},{opacity:1,y:0})
+      tl.current.fromTo(".h3", { opacity:1},{opacity:0})
+
+
+
+    },ref)
+
+
+
+    return () => ctx.revert();
+
+  },[])
+
   return (
     <div className='text-white'>
                <Navbar />
-
-        Home
+        <HomeTextAnime  refe={ref}/>
+      
         </div>
   )
 }
